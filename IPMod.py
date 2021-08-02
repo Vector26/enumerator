f6998b880996cff3012d8d27d90df443252efa95
vector26 <mrdev2604@gmail.com>
2021-08-02 22:28:00 +0530

    init commit

--- IPMod.py
+++ IPMod.py
@@ -3,12 +3,10 @@ class IPMod():
     ip=tuple()
     scanResults=dict()
     def __init__(self,tempIP):
-        # print(tempIP)
         self.ip=self.fromString(tempIP)
         print(self.ip)
     def scan(self):
         nm = nmap.PortScanner()
-        print(f"{self.ip}")
         self.scanResults=nm.scan(f"{self.toString()}", '22-10000')
         print(self.scanResults)
     def toString(self):
@@ -17,5 +15,4 @@ class IPMod():
         return str
     def fromString(self,IP):
         res = tuple(map(str,IP.split('.')))
-        # print(res)
         return res
